c109abe0f9ee2a0741c8d943861b27105c2379ec
Frank <frank@seed.run>
2019-10-06 19:28:04 -0400

    Initial commit

--- services/notes-api/list.js
+++ services/notes-api/list.js
@@ -3,7 +3,7 @@ import { success, failure } from "../../libs/response-lib";
 
 export async function main(event, context) {
   const params = {
-    TableName: "mono-notes",
+    TableName: "ext-notes",
     // 'KeyConditionExpression' defines the condition for the query
     // - 'userId = :userId': only return items with matching 'userId'
     //   partition key
