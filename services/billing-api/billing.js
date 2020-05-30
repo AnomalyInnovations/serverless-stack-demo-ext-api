7e0381d370501800a4bb8ece6257497e1c6439d9
Frank <wangfanjie@gmail.com>
2020-05-30 19:38:12 -0400

    Update

--- services/billing-api/billing.js
+++ services/billing-api/billing.js
@@ -1,4 +1,4 @@
-jmport AWS from "../../libs/aws-sdk";
+import AWS from "../../libs/aws-sdk";
 import stripePackage from "stripe";
 import { calculateCost } from "./libs/billing-lib";
 import { success, failure } from "../../libs/response-lib";
